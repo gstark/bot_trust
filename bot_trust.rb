ee52552e33e44d906ecd0f259987dba581220590
Gavin Stark <g.stark@realdigitalmedia.com>
2011-05-31 18:41:21 -0400

    Basic Robot class

--- bot_trust.rb
+++ bot_trust.rb
@@ -1,11 +1,29 @@
+class Robot
+  attr_accessor :buttons
+
+  def initialize
+    @buttons = []
+  end
+end
+
 class Coordinator
   def initialize(data_string)
     elements = data_string.split
 
     @number_of_buttons = elements.shift
-    elements.each_slice(2) do |robot,button|
-      puts [robot,button].inspect
+    elements.each_slice(2) do |robot_color,button|
+      robot_for_color(robot_color).buttons << button
     end
+
+    puts robots.inspect
+  end
+
+  def robot_for_color(color)
+    robots[color] ||= Robot.new
+  end
+
+  def robots
+    @robots ||= {}
   end
 end
 
